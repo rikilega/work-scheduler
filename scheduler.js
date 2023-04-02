d6d0f2cbbb764325560363033f193f52cf0e0065
rikilega <rosedoland33@yahoo.com>
2023-04-02 12:40:31 -0700

    added function to compare .time-block hour to the currentHour and add relevant class to the time-block as well as text area.

--- scheduler.js
+++ scheduler.js
@@ -4,4 +4,21 @@ $(document).ready(function () {
     $("#currentDay").text(currentDay);
 
     var currentHour = dayjs().hour();
+
+    //Loop through time blocks and set class based on current hour block
+    $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).find("textarea").addClass("past")
+        } else if (blockHour === currentHour) {
+            $(this).addClass("present");
+            $(this).find("textarea").addClass("present");
+        } else {
+            $(this).addClass("future");
+            $(this).find("textarea").addClass("future");
+        }
+    })
 });
+
+
