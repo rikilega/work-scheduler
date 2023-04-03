14353a89f1446dd9c4744455143a018c9fb732b2
rikilega <rosedoland33@yahoo.com>
2023-04-02 20:04:04 -0700

    added function and validation for savebutton and confirmation message/alert messages. added function for edit button. fixed styling, outlines, location of the current date and time

--- scheduler.js
+++ scheduler.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-    var currentDay = dayjs().format("dddd, MMMM D, YYYY");
+    var currentDay = dayjs().format("dddd, MMMM D, YYYY h:mm A");
     $("#currentDay").text(currentDay);
 
     var currentHour = dayjs().hour();
@@ -21,11 +21,11 @@ $(document).ready(function () {
     });
 
     //Load any saved entries from local storage
-    $(".time-block").each(function () {
+    $(".time-block").each(function() {
         var time = $(this).attr("id");
         var text = localStorage.getItem(time);
-        var textarea = $("this").children("description").children("textarea");
-
+        var textarea = $(this).children(".description").children("textarea");
+        
         if (text) {
             textarea.val(text)
             textarea.attr("disabled", true)
@@ -34,10 +34,29 @@ $(document).ready(function () {
 
     //save entry to local storage and disable the text area once saved
     $(".saveBtn").on("click", function() {
-        var time = $(this).parent().attr("id");
-        var text = $(this).siblings(".description").children("textarea").val().trim();
-        localStorage.setItem(time, text);
-        $(this).siblings(".description").children("textarea").attr("disabled", true); // add disabled attribute to textarea
+        
+        var savedConf = $("header").children("#saved-entry");
+        var textarea = $(this).siblings(".description").children("textarea");
+        if (textarea.prop("disabled")) {
+            savedConf.css("display", "none")
+            alert("Please click the edit button and make changes before saving.");
+        }   else if (textarea.val() === '') {
+            alert("There is nothing to save")
+        }   else {
+            savedConf.css("display", "block")
+            var time = $(this).parent().attr("id");
+            var text = $(this).siblings(".description").children("textarea").val().trim();
+            localStorage.setItem(time, text);
+        }
+
+        
+        setTimeout (() => {
+            savedConf.css("display", "none");
+        }, 1000);
+
+   
+
+        $(this).siblings(".description").children("textarea").attr("disabled", true); // add disabled attribute to textarea;
     });
     //allow editing of a saved entry.
     $(".editBtn").on("click", function() {
